c627fe4f0c48a9f39b7dc5c8339f6594442d96f4
Justin <justinjdbennett@gmail.com>
2023-01-03 18:44:18 -0700

    Workng login button and re-install cookie-session

--- routes/login.js
+++ routes/login.js
@@ -8,8 +8,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/:id', (req, res) => {
+  console.log('req.session.user_id BEFORE ➡️ ', `${req.session.user_id}`);
+
   // Set to 1 for simple login and functionality testing
-  req.session.user_id = req.params.id;
+  req.session.user_id = 1;
+  console.log('req.params.id ➡️ ', `${req.params.id}`);
+  console.log('req.session.user_id ➡️ ', `${req.session.user_id}`);
 
   res.redirect('/');
 });
