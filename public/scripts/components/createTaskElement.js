41875dfd06d34be18f2236c033adeefc79c55b2f
Justin <justinjdbennett@gmail.com>
2023-01-04 16:39:41 -0700

    Add component to create task element

---
+++ public/scripts/components/createTaskElement.js
@@ -0,0 +1,23 @@
+/**
+ * Takes in task object to parse data for task element.
+ * @param {{}} task
+ * TODO: Initialize in client.js
+ */
+$(() => {
+  const createTaskElement = function(task) => {
+    return `
+  <li>
+    <input type="checkbox" />${task.description}
+    <button
+      class="collapse-btn"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#collapseExample"
+      aria-expanded="false"
+      aria-controls="collapseExample">
+      <i class="fa-solid fa-angles-down"></i>
+    </button>
+  </li>
+  `;
+  };
+});
