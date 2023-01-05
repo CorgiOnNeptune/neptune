3d3ab62e8eb9451f83461ee78013c316cb6a6c89
Justin <justinjdbennett@gmail.com>
2023-01-04 18:14:57 -0700

    Correct merge conflicts

--- public/scripts/components/createTaskElement.js
+++ public/scripts/components/createTaskElement.js
@@ -6,7 +6,13 @@ $(() => {
   function createTaskElement(task) {
     return `
   <li>
-    <input type="checkbox" />${task.description}
+    <div class="task-content">
+      <input type="checkbox"/>
+      <span>${task.description}</span>
+      <span class="edit-delete-section">
+        <span class="edit-delete"><button><i class="fa-solid fa-pen-to-square"></i></button></span>
+        <span class="edit-delete"><button><i class="fa-sharp fa-solid fa-trash"></i></button></span>
+      </span></div>
     <button
       class="collapse-btn"
       type="button"
