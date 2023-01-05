e45c198a44378703a20a439475bafc3564ed077a
Likai Liu <likailiu2@gmail.com>
2023-01-05 15:20:21 +0800

    Move the animation function for the complete status icon to client-helpers and invoke it in client

--- public/scripts/client-helpers.js
+++ public/scripts/client-helpers.js
@@ -10,3 +10,19 @@ const submitNewTask = (element) => {
       console.log(err.message);
     });
 };
+
+const completeStatusAnimation = function() {
+  $(".complete-status").click(() => {
+    $(".complete-status").fadeOut(250, function() {
+      if ($(this).attr("src") === "images/not-completed.png") {
+        $(this).attr("src", "images/completed.png");
+        $(this).removeClass("not-completed");
+        $(this).addClass("completed");
+      } else {
+        $(this).attr("src", "images/not-completed.png");
+        $(this).removeClass("completed");
+        $(this).addClass("not-completed");
+      }
+    }).fadeIn(250);
+  });
+};
