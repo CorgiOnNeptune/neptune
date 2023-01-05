07a4a5ca422657aa5b1815747356e8722070615d
Justin <justinjdbennett@gmail.com>
2023-01-05 13:40:04 -0700

    reload tasks on post and formatDate() function

--- public/scripts/client-helpers.js
+++ public/scripts/client-helpers.js
@@ -3,6 +3,8 @@ const submitNewTask = (element) => {
 
   $.post('/tasks', $form.serialize())
     .then(() => {
+      $("#tasks-ul").empty();
+      loadTasks('incomplete');
       $form[0].reset();
       closeEditor($('.editor'));
     })
@@ -11,9 +13,9 @@ const submitNewTask = (element) => {
     });
 };
 
-const completeStatusAnimation = function() {
-  $(".complete-status").click(function() {
-    $(this).fadeOut(250, function() {
+const completeStatusAnimation = function () {
+  $(".complete-status").click(function () {
+    $(this).fadeOut(250, function () {
       if ($(this).attr("src") === "images/not-completed.png") {
         $(this).attr("src", "images/completed.png");
         $(this).removeClass("not-completed");
@@ -27,7 +29,7 @@ const completeStatusAnimation = function() {
   });
 };
 
-const escape = function(str) {
+const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
@@ -43,7 +45,7 @@ const createTaskElement = (task) => {
     completeStatus = "not-completed";
     iconSrc = "images/not-completed.png";
   }
-  const dueDate = task.due_date.slice(0, 10);
+  const dueDate = formatDate(task.due_date.slice(0, 10));
   let iconType = `<i class="fa-solid fa-clipboard category-icon"></i>`;
   if (task.category === "films") {
     iconType = `<i class="fa-solid fa-video category-icon"></i>`;
@@ -64,28 +66,28 @@ const createTaskElement = (task) => {
   const $task = $(`
   <li id="task_id_${task.id}">
     <div class="task-content">
-                  <img src="${iconSrc}" alt="" class="complete-status ${completeStatus}">
-                ${iconType}
-                <span>${escape(task.task_name)}</span>
-                <span class="edit-delete-section">
-                  <span class="due-date">Due ${dueDate}</span>
-                  <span class="edit-delete"><button data-modal-target="#old-task-editor"><i class="fa-solid fa-pen-to-square"></i></button></span>
-                  <span class="edit-delete"><button><i class="fa-sharp fa-solid fa-trash"></i></button></span>
-                </span>
+      <img src="${iconSrc}" alt="" class="complete-status ${completeStatus}">
+      ${iconType}
+      <span>${escape(task.task_name)}</span>
+      <span class="edit-delete-section">
+        <span class="due-date">Due ${dueDate}</span>
+        <span class="edit-delete"><button data-modal-target="#old-task-editor"><i class="fa-solid fa-pen-to-square"></i></button></span>
+        <span class="edit-delete"><button><i class="fa-sharp fa-solid fa-trash"></i></button></span>
+      </span>
     </div>
   </li>
   `);
   return $task;
 };
 
-const renderTasks = function(tasks) {
+const renderTasks = function (tasks) {
   for (let task of tasks) {
     const $task = createTaskElement(task);
     $('#tasks-ul').prepend($task);
   }
 };
 
-const loadTasks = function(category) {
+const loadTasks = function (category) {
   let url = "/tasks";
   if (category) {
     url += `/${category}`;
@@ -105,8 +107,8 @@ const loadTasks = function(category) {
     });
 };
 
-const loadTasksByCategory = function() {
-  $(".menu-category").click(function() {
+const loadTasksByCategory = function () {
+  $(".menu-category").click(function () {
     $("#tasks-ul").empty();
 
     if ($(this).hasClass("all-tasks")) {
@@ -142,3 +144,24 @@ const loadTasksByCategory = function() {
     }
   });
 };
+
+/**
+ * Takes in a date string "YYYY-MM-DD" converts it to 'Month DD, YYYY'
+ * @param {string} date
+ */
+
+const formatDate = (date) => {
+  if (!date) return;
+
+  const dateArr = date.split('-');
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  let month = dateArr[1];
+
+  months.forEach((val, index) => {
+    if (index + 1 === Number(month)) {
+      month = months[index];
+    }
+  })
+
+  return `${month} ${dateArr[2]}, ${dateArr[0]}`;
+}
