61ac1fdbec2b198bd0a5aca534436d7308c0c73f
Justin <justinjdbennett@gmail.com>
2023-01-06 02:42:52 -0700

    Adjust google books api get

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -29,7 +29,7 @@ router.get('/omdb/:query', (req, res, next) => {
 router.get('/gbooks/:query', (req, res, next) => {
   const query = req.params.query;
 
-  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}+filter=full+startIndex=0+maxResults=5`)
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}`)
     .then(data => {
       console.log('GBooks Request Complete');
       // console.log(data);
