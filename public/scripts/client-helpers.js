7215a9b3423722df95ae6d13bbc741338fb68e1b
Likai Liu <likailiu2@gmail.com>
2023-01-06 19:08:59 +0800

    Add a helper function to set the default due date of the new task form to today

--- public/scripts/client-helpers.js
+++ public/scripts/client-helpers.js
@@ -241,3 +241,21 @@ const formatDate = (date) => {
 
   return `${month} ${dateArr[2]}, ${dateArr[0]}`;
 };
+
+const setDefaultDate = function() {
+  const date = new Date();
+
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  if (month < 10) {
+    month = "0" + month;
+  }
+  if (day < 10) {
+    day = "0" + day;
+  }
+
+  const today = year + "-" + month + "-" + day;
+  $("#due_date").attr("value", today);
+};
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,4 +11,6 @@ $(() => {
   loadTasks("incomplete");
 
   loadTasksByCategory();
+
+  setDefaultDate();
 });
