585d97122430f6aaea0b578b6076d0965be27668
Likai Liu <likailiu2@gmail.com>
2023-01-09 22:54:57 +0800

    Eslint errors clean-up

--- public/scripts/api-requests.js
+++ public/scripts/api-requests.js
@@ -2,6 +2,65 @@
  * Functions that make async requests using routes to APIs for data
  */
 
+/* global timeoutPromise */
+
+/**
+ * Makes a GET request to the OMDB API
+ * @param  {string}      query URI encoded search query sent to API
+ * @return {Promise<{}>}       Object with data from the API
+ */
+const makeOMDBRequest = (query) =>
+  $.get(`/api/omdb/${query}`).fail((err) => console.log(err.message));
+
+/**
+ * Makes a GET request to the TMDB API
+ * @param  {string}      query URI encoded search query sent to API
+ * @return {Promise<{}>}       Object with data from the API
+ */
+const makeTMDBRequest = (query) =>
+  $.get(`/api/tmdb/${query}`).fail((err) => console.log(err.message));
+
+/**
+ * Makes a GET request to the Google Books API
+ * @param  {string}      query URI encoded search query sent to API
+ * @return {Promise<{}>}       Object with data from the API
+ */
+const makeGBooksRequest = (query) =>
+  $.get(`/api/gbooks/${query}`).fail((err) => console.log(err.message));
+
+/**
+ * Makes a GET request to the IP API to get user location
+ * @return {Promise<{}>} Object with location data from the API
+ */
+const makeIPRequest = () =>
+  $.get(`/api/ipapi`).fail((err) => console.log(err.message));
+
+/**
+ * Makes a GET request to the Yelp API using location from IP API
+ * @param  {string}      query URI encoded search query sent to API
+ * @return {Promise<{}>}       Object with data from the API
+ */
+const makeYelpRequest = (query) =>
+  makeIPRequest()
+    .then((location) => {
+      const latitude = location.latitude;
+      const longitude = location.longitude;
+
+      return $.get(`/api/yelp/${query}/${latitude}/${longitude}`).fail((err) =>
+        console.log(err.message)
+      );
+    })
+    .fail((err) => console.log(err.message));
+
+/**
+ * Makes a GET request to the Amazon Price API
+ * @TODO Replace with an API that doesn't have a 150 request limit...
+ * @param  {string}      query URI encoded search query sent to API
+ * @return {Promise<{}>}       Object with data from the API
+ */
+const makeAMZNRequest = (query) =>
+  $.get(`/api/amazonprice/${query}`).fail((err) => console.log(err.message));
+
 /**
  * Makes a GET call to all available APIs
  * API calls are rejected if they don't resolve before the timeout
@@ -9,7 +68,7 @@
  * @return {Promise<{}>}  Returns a promise to the APIs including the result
  *                        and desired data from each API
  */
-const makeAPIRequests = query => {
+const makeAPIRequests = (query) => {
   const encodedQuery = encodeURIComponent(query);
   console.log('encodedQuery ➡️ ', `${encodedQuery}`);
 
@@ -20,82 +79,15 @@ const makeAPIRequests = query => {
     timeoutPromise(5000, makeYelpRequest(encodedQuery)),
     timeoutPromise(5000, makeOMDBRequest(encodedQuery)),
     timeoutPromise(5000, makeTMDBRequest(encodedQuery)),
-    timeoutPromise(5000, makeGBooksRequest(encodedQuery))
+    timeoutPromise(5000, makeGBooksRequest(encodedQuery)),
     // timeoutPromise(5000, makeAMZNRequest(encodedQuery))
   ];
 
   return Promise.allSettled(requests)
-    .then(results => {
-      console.log('↓ makeAPIRequests() results ↓')
+    .then((results) => {
+      console.log('↓ makeAPIRequests() results ↓');
       console.log(results);
       return results;
     })
-    .catch(err => console.log(err.message));
-};
-
-/**
- * Makes a GET request to the OMDB API
- * @param  {string}      query URI encoded search query sent to API
- * @return {Promise<{}>}       Object with data from the API
- */
-const makeOMDBRequest = query => {
-  return $.get((`/api/omdb/${query}`))
-    .fail(err => console.log(err.message));
-};
-
-/**
- * Makes a GET request to the TMDB API
- * @param  {string}      query URI encoded search query sent to API
- * @return {Promise<{}>}       Object with data from the API
- */
-const makeTMDBRequest = query => {
-  return $.get((`/api/tmdb/${query}`))
-    .fail(err => console.log(err.message));
-};
-
-/**
- * Makes a GET request to the Google Books API
- * @param  {string}      query URI encoded search query sent to API
- * @return {Promise<{}>}       Object with data from the API
- */
-const makeGBooksRequest = query => {
-  return $.get((`/api/gbooks/${query}`))
-    .fail(err => console.log(err.message));
-};
-
-/**
- * Makes a GET request to the Yelp API using location from IP API
- * @param  {string}      query URI encoded search query sent to API
- * @return {Promise<{}>}       Object with data from the API
- */
-const makeYelpRequest = query => {
-  return makeIPRequest()
-    .then(location => {
-      const latitude = location.latitude;
-      const longitude = location.longitude;
-
-      return $.get((`/api/yelp/${query}/${latitude}/${longitude}`))
-        .fail(err => console.log(err.message));
-    })
-    .fail(err => console.log(err.message));
-};
-
-/**
- * Makes a GET request to the IP API to get user location
- * @return {Promise<{}>} Object with location data from the API
- */
-const makeIPRequest = () => {
-  return $.get((`/api/ipapi`))
-    .fail(err => console.log(err.message));
-};
-
-/**
- * Makes a GET request to the Amazon Price API
- * @TODO Replace with an API that doesn't have a 150 request limit...
- * @param  {string}      query URI encoded search query sent to API
- * @return {Promise<{}>}       Object with data from the API
- */
-const makeAMZNRequest = (query) => {
-  return $.get((`/api/amazonprice/${query}`))
-    .fail(err => console.log(err.message));
+    .catch((err) => console.log(err.message));
 };
