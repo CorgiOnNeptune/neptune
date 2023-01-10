21e9ae033af5d1421b59a70b1b3771e703e844f0
Justin <justinjdbennett@gmail.com>
2023-01-09 21:35:29 -0700

    Update id name

--- public/scripts/client-helpers.js
+++ public/scripts/client-helpers.js
@@ -431,12 +431,12 @@ const createTaskElement = (task) => {
     class="collapse-btn"
     type="button"
     data-bs-toggle="collapse"
-    data-bs-target="#collapse${id}"
+    data-bs-target="#collapseDetails${id}"
     aria-expanded="false"
-    aria-controls="collapse${id}">
+    aria-controls="collapseDetails${id}">
     <i class="fa-solid fa-angles-down"></i>
   </button>
-  <div class="collapse details" id="collapse${id}">
+  <div class="collapse details" id="collapseDetails${id}">
     <div class="poster-container"><img src="https://www.themoviedb.org/t/p/original/1AnfXMG9PPMVjwXcHW6JLSJUbPo.jpg" alt="poster" id="poster"></div>
     <div class="info">
       <div class="info-header">
